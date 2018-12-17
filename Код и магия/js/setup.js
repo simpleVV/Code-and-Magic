72ff6cb11e83004651e31ade2456926b534dc64e
inwork <simpleHelpD@yandex.ru>
2018-12-17 14:50:12 +1000

    Добавление функции(получение случайного значения)

--- Код и магия/js/setup.js
+++ Код и магия/js/setup.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var userDialog = document.querySelector('.setup');
@@ -9,36 +10,45 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .querySelector('.setup-similar-item');
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARD_NUMBER = 4;
 
-// Генерация случайного образа волшебника
-var wizardGeneration = function (names, secondNames, coatColors, eyesColors) {
+// Функия получения случайного значения
+var getRandomValue = function (max, min) {
+  if (min) {
+    return Math.floor(Math.random() * (max)) + min;
+  } else {
+    return Math.floor(Math.random() * (max));
+  }
+};
+
+// Создание случайного образа волшебника
+var getDifferentWizard = function (names, secondNames, coatColors, eyesColors) {
   var wizardsData = [];
-  var randomNumber = Math.floor(Math.random() * 10); // Для перестановки имени и фамилии местами
+  var randomNumber = getRandomValue(10); // Для перестановки имени и фамилии местами
   var fullName = '';
 
-  // Случайно меняем местами имя и фамилию
+  // Меняем местами имя и фамилию
   for (var i = 0; i < WIZARD_NUMBER; i++) {
     if (randomNumber > WIZARD_NUMBER) {
-      fullName = names[Math.floor(Math.random() * names.length)] + ' ' + secondNames[Math.floor(Math.random() * secondNames.length)];
+      fullName = names[getRandomValue(names.length)] + ' ' + secondNames[getRandomValue(secondNames.length)];
     } else {
-      fullName = secondNames[Math.floor(Math.random() * secondNames.length)] + ' ' + names[Math.floor(Math.random() * names.length)];
+      fullName = secondNames[getRandomValue(secondNames.length)] + ' ' + names[getRandomValue(names.length)];
     }
 
     // Заполняем данными объекты
     var wizard = {
       name: fullName,
-      coatColor: coatColors[Math.floor(Math.random() * coatColors.length)],
-      eyeColor: eyesColors[Math.floor(Math.random() * eyesColors.length)]
+      coatColor: coatColors[getRandomValue(coatColors.length)],
+      eyeColor: eyesColors[getRandomValue(eyesColors.length)]
     };
     wizardsData.push(wizard);
   }
   return wizardsData;
 };
 
-var wizards = wizardGeneration(WIZARD_NAMES, WIZARD_SECOND_NAMES, WIZARD_COAT_COLOR, WIZARD_EYES_COLOR);
+var wizards = getDifferentWizard(WIZARD_NAMES, WIZARD_SECOND_NAMES, WIZARD_COAT_COLORS, WIZARD_EYES_COLOR);
 
 // Создание DOM элементов на основе сгенерированных образов волшебников
 var renderWizard = function (wizard) {
